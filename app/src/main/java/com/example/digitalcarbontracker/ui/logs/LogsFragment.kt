f24dbd293ed29e7c547bbc6fa5bc758f31a0d8df
Kratika444 <kratikanigam25@gmail.com>
2026-02-18 22:22:22 +0530

    new log fragment layout file added

--- app/src/main/java/com/example/digitalcarbontracker/ui/logs/LogsFragment.kt
+++ app/src/main/java/com/example/digitalcarbontracker/ui/logs/LogsFragment.kt
@@ -1,1 +1,32 @@
+package com.example.click2co2meter3.ui.logs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.click2co2meter3.databinding.FragmentLogsBinding
+
+class LogsFragment : Fragment() {
+
+    private var _binding: FragmentLogsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLogsBinding.inflate(inflater, container, false)
+
+        binding.tvTitle.text = "📑 Logs"
+        binding.tvDesc.text = "Collected device usage logs will appear here."
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
 
