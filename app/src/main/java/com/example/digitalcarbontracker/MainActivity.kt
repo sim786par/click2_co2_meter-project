1d681dbc224baa49f206f0d42ee7332606c0ce2b
sim786par <simranparween2754@gmail.com>
2026-02-18 20:31:30 +0530

    MainActivity code added

--- app/src/main/java/com/example/digitalcarbontracker/MainActivity.kt
+++ app/src/main/java/com/example/digitalcarbontracker/MainActivity.kt
@@ -1,1 +1,41 @@
+package com.example.digitalcarbontracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.digitalcarbontracker.databinding.ActivityMainBinding
+import com.example.digitalcarbontracker.worker.EmissionWorker
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNav.setupWithNavController(navController)
+
+        startWorker()
+    }
+
+    private fun startWorker() {
+        val workRequest = PeriodicWorkRequestBuilder<EmissionWorker>(15, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "EmissionWorker",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            workRequest
+        )
+    }
+}
 
