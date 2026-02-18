282a3c49e3bcc86366be246276a9f5973d5b0e77
Kratika444 <kratikanigam25@gmail.com>
2026-02-18 22:24:19 +0530

    tips fragment layout file added

--- app/src/main/java/com/example/digitalcarbontracker/ui/tips/TipsFragment.kt
+++ app/src/main/java/com/example/digitalcarbontracker/ui/tips/TipsFragment.kt
@@ -1,1 +1,31 @@
+package com.example.click2co2meter3.ui.tips
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.click2co2meter3.databinding.FragmentTipsBinding
+
+class TipsFragment : Fragment() {
+
+    private var _binding: FragmentTipsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTipsBinding.inflate(inflater, container, false)
+
+        binding.tvTitle.text = "🌱 Eco Tips"
+        binding.tvDesc.text = "Suggestions to reduce digital carbon footprint will appear here."
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
