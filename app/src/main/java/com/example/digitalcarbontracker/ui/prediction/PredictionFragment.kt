ff848e2561db5a2b0bad6599af7f4414ca7608a1
Kratika444 <kratikanigam25@gmail.com>
2026-02-18 22:23:35 +0530

    prediction fragment layout file added

--- app/src/main/java/com/example/digitalcarbontracker/ui/prediction/PredictionFragment.kt
+++ app/src/main/java/com/example/digitalcarbontracker/ui/prediction/PredictionFragment.kt
@@ -1,1 +1,32 @@
+package com.example.click2co2meter3.ui.prediction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.click2co2meter3.databinding.FragmentPredictionBinding
+
+class PredictionFragment : Fragment() {
+
+    private var _binding: FragmentPredictionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPredictionBinding.inflate(inflater, container, false)
+
+        binding.tvTitle.text = "🤖 Prediction"
+        binding.tvDesc.text = "AI-based prediction will be implemented here."
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
 
