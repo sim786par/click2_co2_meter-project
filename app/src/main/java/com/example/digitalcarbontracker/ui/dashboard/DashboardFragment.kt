e967a28c90b3d96bd91d746c1b583e45a348d740
Kratika444 <kratikanigam25@gmail.com>
2026-02-18 22:25:22 +0530

    new dashboard fragment layout file added

--- app/src/main/java/com/example/digitalcarbontracker/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/digitalcarbontracker/ui/dashboard/DashboardFragment.kt
@@ -1,64 +1,31 @@
-package com.example.click2co2meter2.ui.dashboard
+package com.example.click2co2meter3.ui.dashboard
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.click2co2meter2.R
+import com.example.click2co2meter3.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private var initialBatteryLevel = 0
-    private lateinit var tvCarbon: TextView
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding.tvTitle.text = "📊 Dashboard"
+        binding.tvDesc.text = "Daily / Monthly / Yearly CO₂ emission will be shown here."
 
-        tvCarbon = view.findViewById(R.id.tvCarbon)
-
-        getBatteryLevel()
-
-        return view
+        return binding.root
     }
 
-    private fun getBatteryLevel() {
-
-        val batteryStatus = requireContext().registerReceiver(
-            null,
-            IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        )
-
-        val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
-
-        if (initialBatteryLevel == 0) {
-            initialBatteryLevel = level
-        }
-
-        val batteryUsed = initialBatteryLevel - level
-
-        val co2 = calculateCarbon(batteryUsed)
-
-        tvCarbon.text = "$co2 g CO2"
-    }
-
-    private fun calculateCarbon(batteryPercent: Int): Double {
-
-        val energyWh = batteryPercent * 0.05
-        val energyKWh = energyWh * 0.001
-        val carbon = energyKWh * 700
-
-        return String.format("%.2f", carbon).toDouble()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
